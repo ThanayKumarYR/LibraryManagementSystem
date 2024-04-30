1efb564103164ba90213c6554cf3a61a58a063d8
Thanay Kumar Y R <thanaykumaryr@gmail.com>
2024-04-30 15:04:51 +0530

    Created book via its interface

---
+++ LibrarySystem/ILibraryItem.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibrarySystem
+{
+    public interface ILibraryItem
+    {
+        string Title { get; }
+        string Author { get; }
+    }
+}
--- LibrarySystem/Program.cs
+++ LibrarySystem/Program.cs
@@ -8,9 +8,15 @@ namespace LibrarySystem
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
             Console.WriteLine("Geting started with Library Management System !");
+
+            Book MalgudiDays = new Book("MalgudiDays", "R. K. Narayan", "9788185986173");
+            Book RomeoAndJuliet = new Book("Romeo and Juliet", "William Shakespeare", "9789380816272");
+
+            Console.WriteLine($"Book: {MalgudiDays.Title} Author: {MalgudiDays.Author} Isbn: {MalgudiDays.ISBN}");
+            Console.WriteLine($"Book: {RomeoAndJuliet.Title} Author: {RomeoAndJuliet.Author} Isbn: {RomeoAndJuliet.ISBN}");
         }
     }
 }
