8928f49ea65b582b596c3f4857867533a65bb098
Thanay Kumar Y R <thanaykumaryr@gmail.com>
2024-04-30 15:21:18 +0530

    add: user via its interface

---
+++ LibrarySystem/LibraryUser.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibrarySystem
+{
+    public class LibraryUser : ILibraryUser
+    {
+        public string Name { get; set; }
+        public int ID { get; set; }
+
+        public LibraryUser(string name, int id)
+        {
+            Name = name;
+            ID = id;
+        }
+    }
+}
--- LibrarySystem/Program.cs
+++ LibrarySystem/Program.cs
@@ -12,11 +12,18 @@ namespace LibrarySystem
         {
             Console.WriteLine("Geting started with Library Management System !");
 
-            Book MalgudiDays = new Book("MalgudiDays", "R. K. Narayan", "9788185986173");
-            Book RomeoAndJuliet = new Book("Romeo and Juliet", "William Shakespeare", "9789380816272");
+            Book Book_MalgudiDays = new Book("Book_MalgudiDays", "R. K. Narayan", "9788185986173");
+            Book Book_RomeoAndJuliet = new Book("Romeo and Juliet", "William Shakespeare", "9789380816272");
 
-            Console.WriteLine($"Book: {MalgudiDays.Title} Author: {MalgudiDays.Author} Isbn: {MalgudiDays.ISBN}");
-            Console.WriteLine($"Book: {RomeoAndJuliet.Title} Author: {RomeoAndJuliet.Author} Isbn: {RomeoAndJuliet.ISBN}");
+            Console.WriteLine($"Book: {Book_MalgudiDays.Title} Author: {Book_MalgudiDays.Author} Isbn: {Book_MalgudiDays.ISBN}");
+            Console.WriteLine($"Book: {Book_RomeoAndJuliet.Title} Author: {Book_RomeoAndJuliet.Author} Isbn: {Book_RomeoAndJuliet.ISBN}");
+
+            LibraryUser User_Thanay = new LibraryUser("Thanay", 1);
+            LibraryUser User_Saikat = new LibraryUser("Saikat", 2);
+
+            Console.WriteLine($"userId: {User_Thanay.ID} UserName: {User_Thanay.Name}");
+            Console.WriteLine($"userId: {User_Saikat.ID} UserName: {User_Saikat.Name}");
+
         }
     }
 }
